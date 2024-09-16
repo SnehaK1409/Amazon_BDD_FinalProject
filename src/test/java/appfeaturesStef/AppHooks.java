fff93b3ab1fe941b06c4dac16043df4b390a02aa
SnehaK <khambesnehal09@gmail.com>
2024-09-16 15:15:44 +0530

    Sneha added new class

---
+++ src/test/java/appfeaturesStef/AppHooks.java
@@ -0,0 +1,53 @@
+package appfeaturesStef;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import driverFactoryClass.DriverFactory;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+public class AppHooks 
+{
+
+	WebDriver driver;
+	DriverFactory df;
+	@Before
+	public void initiateBrowser() throws IOException
+	{
+		Properties prop=new Properties(); 
+		String path =System.getProperty("user.dir")+"//src//test//resources//config.properties";
+		FileInputStream fis=new FileInputStream(path);
+		prop.load(fis);
+		String browserName = prop.getProperty("browser");
+		df=new DriverFactory();
+		driver=df.initBrowser(browserName);
+		driver.manage().window().maximize();
+	}
+	
+	@After
+	public void tearDown()
+	{
+		driver.quit();
+	}
+	
+	@After
+	public void afterScenario(Scenario scenario) throws IOException
+	{
+		if(scenario.isFailed())
+		{
+			TakesScreenshot ts=(TakesScreenshot)driver;
+			File screenshotFile = ts.getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(screenshotFile, new File("E:\\Advance Testing Techologies\\"+scenario.getName()+".png"));
+		}
+	}
+	
+}
---
+++ src/test/resources/appfeatures/appfeature.feature
@@ -0,0 +1,16 @@
+Feature: Home Page Functionality
+
+Scenario: Verfiy the title of page
+Given User is at amazon portal 
+Then Title should contains the "Shopping" 
+
+Scenario: Verfiy the cart icon
+Given User is at amazon portal 
+Then User should be able to see cart icon
+
+Scenario: Verify the sign in page
+Given User is at amazon portal
+When user should click in Sign in button 
+And User should enter the username as "7755996082"
+And User should click on continue button of amazon 
+And User should enter the password as "Kishor@428"
